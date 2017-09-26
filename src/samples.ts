5b5e9e9233df62054f3bf2321a21ce7d2c5cfc9a
Josh Goldberg <josh@fullscreenmario.com>
2017-09-26 10:19:27 -0400

    Fleshed out samples

--- src/samples.ts
+++ src/samples.ts
@@ -13,83 +13,96 @@ const prepare = (strings: TemplateStringsArray): string => {
     return source.substring(source.search(/\S+/));
 };
 
-/* tslint:disable */
+/* tslint:disable max-line-length object-literal-sort-keys */
 
 export const Samples = {
-    Default: prepare`
+    "Default": prepare`
 file start : Program File
-    main start
+    main context start
         print : ("GLS is awesome!")
-        print : ("Select a sample above to see how it works!")
-    main end
+        print : ("Select a sample below to see how it works!")
+    main context end
 file end`,
-    Variables: prepare`
+    "Variables": prepare`
 file start : Program File
-    main start
-        comment line : Simple declarations
-        variable : foo string
-        variable : bar number 7
-        
-        comment line : Assignments
-        variable : qux string foo
-        variable : baz number bar
-        
-        comment line : Interesting values
-        variable : quux number infinity
-        variable : corge boolean true
-    main end
+    main context start
+        main start
+            comment line : Simple declarations
+            variable : foo string
+            variable : bar number 7
+
+            comment line : Assignments
+            variable : qux string foo
+            variable : baz number bar
+
+            comment line : Interesting values
+            variable : quux number infinity
+            variable : corge boolean true
+
+            comment line : Operation chains
+            operation : foo (multiply by) 2
+            operation : foo (decrease by) bar times { parenthesis : { operation : bar minus 3 } }
+            variable : bar number { operation : foo (divide by) 3 plus 4 times foo }
+        main end
+    main context end
 file end`,
-    Operations: prepare`
+    "Conditionals": prepare`
 file start : Program File
-    main start
-        variable : foo number 7
-        
-        comment line : Operation chains
-        operation : foo (multiply by) 2
-        operation : foo (decrease by) bar times { parenthesis : { operation : bar minus 3 } }
-        variable : bar number { operation : foo (divide by) 3 plus 4 times foo }
-        
-    main end
-file end`,
-    Conditionals: prepare`
-file start : Program File
-    main start
-        comment line : Conditionals
-        if start : { operation : 2 plus 2 (equal to) 4 }
-            print : ("Seems legit")
-        else if start : { operation : 2 plus 2 (equal to) 5}
-            print : ("That seems wrong...")
-        else start
-            print : ("Math.")
-        if end
-    main end
+    main context start
+        main start
+            comment line : Conditionals
+            if start : { operation : 2 plus 2 (equal to) 4 }
+                print : ("Seems legit")
+            else if start : { operation : 2 plus 2 (equal to) 5}
+                print : ("That seems wrong...")
+            else start
+                print : ("Math.")
+            if end
+        main end
+    main context end
 file end`,
     "Arrays and Lists": prepare`
 file start : Program File
-    main start
-        comment line : Coming soon!
-    main end
+    main context start
+        main start
+            variable : fruits { array type : string } { array initialize : string "apple" "banana" "cherry" }
+
+            print : { string format : ("There are {0} fruits.") { array length : fruits } int }
+            print : { string format : ("The first fruit is {0}.") { index : fruits 0 } string }
+
+            variable : vegetables { list type : string } { list initialize : string "artichoke" "broccolli" "carrot" }
+
+            list push : vegetables "onion"
+            list add list : vegetables { list initialize : string "pea" "potato" }
+
+            print : { string format : ("There are {0} vegetables.") { list length : vegetables } int }
+            print : { string format : ("The first vegetable is {0}.") { index : vegetables 0 } string }
+            print : { string format : ("The last vegetable is {0}.") { index : vegetables { operation : { list length : vegetables } minus 1 } } string }
+        main end
+    main context end
 file end`,
-    Dictionaries: prepare`
+    "Dictionaries": prepare`
 file start : Program File
     main start
-        comment line : Dictionary types
-        variable : foo { dictionary type : string int } { dictionary new : string int }
-        variable : bar { dictionary type : string { dictionary type : string int } } { dictionary new : string { dictionary new : string int } }
-        
+        comment line : Simple type
+        variable start : respondentResults { dictionary type : string float } { dictionary new start : string float }
+            dictionary pair : "javascript" 62.5 ,
+            dictionary pair : "sql" 51.2 ,
+            dictionary pair : "java" 39.7 ,
+            dictionary pair : "c#" 34.1
+        dictionary new end
+
         comment line : Indices
-        operation : { index : foo "baz" } equals 7
-        variable : qux int { index : foo "baz" }
-        
-        comment line : In-place initialization
-        variable start : aaa { dictionary type : string int } { dictionary new start : string int }
-            dictionary pair : "bbb" 1 ,
-            dictionary pair : "ccc" 2 ,
-            dictionary pair : "ddd" 3
+        variable : javascript float { index : surveyResults "javascript" }
+        operation : { index : respondentResults "python" } equals 7
+
+        comment line : Deep type
+        variable start : allSurveys { dictionary type : string { dictionary type : string float } } { dictionary new start : string { dictionary type : string float } }
+            dictionary pair : "respondent" respondentResults
         dictionary new end
     main end
 file end`,
-    Loops: prepare`
+    "Loops": prepare`
 file start : Program File
     main start
         comment line : While
@@ -97,55 +110,53 @@ file start : Program File
             print : "Hey!"
             break
         while end
-        
+
         comment line : For ((range))
         for numbers start : i int 0 10
             print : i
         for numbers end
-        
+
         comment line: For ((collection))
         variable start : container { dictionary type : string int } { dictionary new start : string int }
             dictionary pair : "bbb" 1 ,
             dictionary pair : "ccc" 2 ,
             dictionary pair : "ddd" 3
         dictionary new end
-        
+
         for each pair start : container pair key string value int
             print : { concatenate : ("Looking at ") key  }
         for each end
     main end
 file end`,
-    Lambdas: prepare`
-comment line : Coming soon!`,
-    Classes: prepare`
+    "Classes": prepare`
 file start : Program File
     class start : Person
-        comment doc start
-        comment doc tag : summary The person's name.
-        comment doc end
         member variable declare : private name string
-        
-        comment doc start
-        comment doc tag : summary The person's age.
-        comment doc end
         member variable declare : private age float
-        
-        comment doc start
-        comment doc tag : parameter name The person's name.
-        comment doc tag : parameter age The person's age.
-        comment doc end
+
         constructor start : Person name string age float
             operation : { member variable : private { this } name } equals name
             operation : { member variable : private { this } age} equals age
         constructor end
+
+        member function declare start : public Speak void
+            print : { concatenate : ("Hello! I'm ") { member variable : private { this } greeting } "!" }
+        member function declare end
+
+        member function declare start : public CreateClone Person
+            return : { new : Person { member variable : private { this } name } 0 }
+        member function declare end
     class end
-    
-    comment line : Coming soon: member functions
-    
-    main start
-        comment line : Coming soon: instances
-    main end
+
+    main context start
+        main start
+            variable : person Person { new : Person "Mark" 26.0 }
+            variable : clone Person { member function : public person CreateClone }
+
+            member function : private clone Speak
+        main end
+    main context end
 file end`,
 };
 
-/* tslint:enable */
+/* tslint:enable max-line-length object-literal-sort-keys */
